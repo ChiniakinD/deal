f58383866c769729ba5ac2d0718ca56453cf61b9
ChiniakinD <pilotuiga@gmail.com>
2024-07-22 00:39:30 +0300

    test(): Добавление тестов. 1. Добавил тесты контроллеров. 2. Добавил testcontainers.

---
+++ src/test/java/com/chiniakin/controller/ContractorToRoleControllerTest.java
@@ -0,0 +1,46 @@
+package com.chiniakin.controller;
+
+import com.chiniakin.TestBeans;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest(classes = TestBeans.class)
+public class ContractorToRoleControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void addContractorToRoleShouldBeSuccessfulAdded() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/contractor-to-role/add")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                      {
+                                          "id": "8a7b9a6e-b60b-4a6f-951d-8b6b56a2f8ec",
+                                          "role_id": {
+                                              "id": "BORROWER",
+                                              "name": "Заемщик",
+                                              "category": "BORROWER",
+                                              "isActive": true
+                                          }
+                                      }
+                                """))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @Sql("/insertForTests/insert.sql")
+    public void deleteContractorToRoleShouldBeSuccessfulDeleted() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/contractor-to-role/delete/{id}", "6e6a086f-5e74-49b4-b82b-99135aa4b555"))
+                .andExpect(status().isOk());
+    }
+}
---
+++ src/test/java/com/chiniakin/controller/DealContractorControllerTest.java
@@ -0,0 +1,47 @@
+package com.chiniakin.controller;
+
+import com.chiniakin.TestBeans;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest(classes = TestBeans.class)
+public class DealContractorControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @Sql("/insertForTests/insert.sql")
+    public void saveContractorShouldSuccessfulSaved() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.put("/deal-contractor/save")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(
+                                """
+                                        {
+                                           "id": "561b5244-29ed-4d18-9145-193775a1d0dc",
+                                           "deal_id": "c3d5b726-92e8-4bb4-9826-66f72fce1c4d",
+                                           "contractor_id": "1233",
+                                           "name": "123",
+                                           "main": "true"
+                                         }
+                                         """
+                        ))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @Sql("/insertForTests/insert.sql")
+    public void deleteContractorShouldSuccessfulDeleted() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/deal-contractor/delete/{id}", "8a7b9a6e-b60b-4a6f-951d-8b6b56a2f8ec"))
+                .andExpect(status().isOk());
+    }
+}
